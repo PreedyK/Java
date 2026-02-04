aacc3e7964d80bd4c633fa904c5df5708b1f2e60
Kiryn Preedy <kiryn.preedy@gmail.com>
2026-02-04 09:49:48 -0600

    Add files via upload

--- Student.java
+++ Student.java
@@ -2,20 +2,46 @@ package Java_PreedyK;
 import java.util.Scanner;
 public class Student {
 	String name;
-	String id;
-	int mark1;
-	int mark2;
-	int mark3;
-	int mark4;
-	int total;
+	int regNo;
+	float mark1, mark2, mark3, mark4;
+	float total;
 	float average;
-	String grade;
+	char grade;
 
+	public void calcResults() {
+		total = mark1 + mark2 + mark3 + mark4;
+		average = total/4;
+		
+		if (average >= 90)
+			grade = 'A';
+		else if (average >= 80)
+			grade = 'B';
+		else if (average >= 70)
+			grade = 'C';
+		else if (average >= 60)
+			grade = 'D';
+		else
+			grade = 'F';
+	}
 	
-	
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+	public void display() {
+		System.out.println("Name: " + name);
+		System.out.println("Reg No: " + regNo);
+		System.out.println("Total: " + total);
+		System.out.println("Average: " + average);
+		System.out.println("Grade: " + grade);
 		
 	}
 
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		
 }
+	
+}
+
+
+	
