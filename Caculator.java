44ada319438c6b4a2fc66190c32683bec07da78e
Kiryn Preedy <kiryn.preedy@gmail.com>
2026-02-09 09:50:33 -0600

    Add files via upload

---
+++ Caculator.java
@@ -0,0 +1,38 @@
+package Java_PreedyK;
+import java.util.Scanner;
+public class Caculator {
+	
+
+	 public static int add(int a, int b) {
+		int c = a + b;
+		return c;
+	}
+	 
+	public static int sub(int a, int b) {
+		int c = a - b;
+		return c;
+	}
+	
+	public static int mult(int a, int b) { 
+		int c = a * b;
+		return c;
+	}
+	
+	public static int div(int a, int b) { 
+		int c = a/b;
+		return c; 
+	}
+
+public class Calculate{ 
+	
+	public static void main(String []args) { 
+		Scanner input = new Scanner(System.in);
+		Calculator calc = new Calculator();
+		
+		
+	}
+}
+}
+
+		
+
